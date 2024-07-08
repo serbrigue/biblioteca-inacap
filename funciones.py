039387170fada7f3ffc2b9b5c640c93a0101937e
Sergio Briones <serbrigue@gmail.com>
2024-07-07 23:15:28 -0400

    implementacion funcion verificar, cambio nombre de funciones

--- funciones.py
+++ funciones.py
@@ -2,13 +2,17 @@ import json
 import os
 
 # Pausa la ejecución hasta que el usuario presione enter y luego limpia la pantalla.
-def congelar():
+def pausar_ejecucion():
     input('Presione enter para continuar...')
     os.system('clear')
 
+# Verifica si la lista de libros está vacía.
+def verificar(archivo):
+    return len(archivo) == 0
+
 # Abre un archivo JSON y devuelve su contenido como una lista de diccionarios.
 # Si el archivo no existe, retorna una lista vacía.
-def abrir(archivo):
+def abrir_archivo(archivo):
     try:
         with open(archivo, 'r') as file:
             productos_data = file.read().strip()
@@ -20,7 +24,7 @@ def abrir(archivo):
         return []
 
 # Guarda una lista de diccionarios en un archivo JSON.
-def guardar(archivo, libros):
+def guardar_archivo(archivo, libros):
     try:
         with open(archivo, 'w') as file:
             libros_data = json.dumps(libros, indent=4)
@@ -30,7 +34,7 @@ def guardar(archivo, libros):
 
 # Busca un libro por su título en la lista de libros.
 # Si encuentra el libro, lo devuelve; si no, devuelve None.
-def buscar(nombre_libro, archivo):
+def buscar_libro(nombre_libro, archivo):
     for libro in archivo:
         if libro["titulo"] == nombre_libro:
             return libro
@@ -39,7 +43,7 @@ def buscar(nombre_libro, archivo):
 # Agrega un libro a la lista de libros.
 # Verifica que el año de publicación sea un número positivo.
 # Si el libro ya existe, muestra un mensaje de error.
-def agregar(titulo, autor, fecha, archivo):
+def agregar_libro(titulo, autor, fecha, archivo):
     try:
         fecha = int(fecha)
         if fecha <= 0:
@@ -47,18 +51,18 @@ def agregar(titulo, autor, fecha, archivo):
     except ValueError as e:
         return f'Error: {e}'
 
-    if buscar(titulo, archivo):
+    if buscar_libro(titulo, archivo):
         return "Error: Libro ya existe"
     else:
         archivo.append({"titulo": titulo, "autor": autor, "fecha": fecha})
-        guardar("libros.json", archivo)
+        guardar_archivo("libros.json", archivo)
         return 'Libro agregado exitosamente'
 
 # Modifica la información de un libro existente.
 # Verifica que el año de publicación sea un número positivo.
 # Si el libro no existe, muestra un mensaje de error.
-def modificar(titulo, nuevo_autor, nueva_fecha, archivo):
-    libro = buscar(titulo, archivo)
+def modificar_libro(titulo, nuevo_autor, nueva_fecha, archivo):
+    libro = buscar_libro(titulo, archivo)
     if libro:
         try:
             nueva_fecha = int(nueva_fecha)
@@ -69,30 +73,29 @@ def modificar(titulo, nuevo_autor, nueva_fecha, archivo):
 
         libro["autor"] = nuevo_autor
         libro["fecha"] = nueva_fecha
-        guardar("libros.json", archivo)
+        guardar_archivo("libros.json", archivo)
         return 'Libro modificado exitosamente'
     else:
         return "Error: Libro no encontrado"
 
 # Muestra todos los libros en la lista de libros.
 # Si no hay libros, muestra un mensaje indicando que la lista está vacía.
-def mostrar(archivo):
-    if len(archivo) == 0:
-        return "No hay libros en la librería"
-    
-    resultado = ""
-    for i, libro in enumerate(archivo):
-        resultado += f'{i+1}. Titulo: {libro["titulo"]} - Autor: {libro["autor"]} - Publicacion: {libro["fecha"]}\n'
-    
-    return resultado.strip()
+def mostrar_libros(archivo):
+    if verificar(archivo):
+        return 'No hay libros en la biblioteca'
+    else:
+        resultado = ''
+        for i, libro in enumerate(archivo):
+            resultado += f'{i+1}. Titulo: {libro["titulo"]} - Autor: {libro["autor"]} - Publicacion: {libro["fecha"]}\n'
+        return resultado.strip()
 
 # Elimina un libro de la lista de libros.
 # Si el libro no existe, muestra un mensaje de error.
-def eliminar(titulo, archivo):
-    libro = buscar(titulo, archivo)
+def eliminar_libro(titulo, archivo):
+    libro = buscar_libro(titulo, archivo)
     if libro:
         archivo.remove(libro)
-        guardar("libros.json", archivo)
+        guardar_archivo("libros.json", archivo)
         return 'Libro eliminado exitosamente'
     else:
         return "Error: Libro no encontrado"
@@ -100,7 +103,7 @@ def eliminar(titulo, archivo):
 # Menú interactivo para la gestión de libros en la biblioteca.
 # Permite agregar, modificar, buscar, mostrar y eliminar libros, además de salir del programa.
 def menu():
-    libros = abrir("libros.json")
+    libros = abrir_archivo("libros.json")
     while True:
         print("1. Agregar libro")
         print("2. Modificar libro")
@@ -119,36 +122,47 @@ def menu():
             titulo = input("Ingrese el titulo del libro: ")
             autor = input("Ingrese el autor del libro: ")
             fecha = input("Ingrese la fecha del libro: ")
-            print(agregar(titulo, autor, fecha, libros))
-            congelar()
+            print(agregar_libro(titulo, autor, fecha, libros))
+            pausar_ejecucion()
         elif opcion == 2:
-            titulo = input("Ingrese el titulo del libro: ")
-            autor = input("Ingrese el nuevo autor para el libro: ")
-            fecha = input("Ingrese la nueva fecha para el libro: ")
-            print(modificar(titulo, autor, fecha, libros))
-            congelar()
-        elif opcion == 3:
-            titulo = input("Ingrese el titulo del libro: ")
-            libro = buscar(titulo, libros)
-            if libro:
-                print(f'Titulo: {libro["titulo"]} - Publicación: {libro["fecha"]} - Autor: {libro["autor"]}')
+            if verificar(libros):
+                print('Error: No hay libros en la biblioteca')
             else:
-                print('Error: Libro no encontrado')
-            congelar()
+                titulo = input("Ingrese el titulo del libro: ")
+                autor = input("Ingrese el nuevo autor para el libro: ")
+                fecha = input("Ingrese la nueva fecha para el libro: ")
+                print(modificar_libro(titulo, autor, fecha, libros))
+            pausar_ejecucion()
+        elif opcion == 3:
+            if verificar(libros):
+                print('Error: No hay libros en la biblioteca')
+            else:
+                titulo = input("Ingrese el titulo del libro: ")
+                libro = buscar_libro(titulo, libros)
+                if libro:
+                    print(f'Titulo: {libro["titulo"]} - Publicación: {libro["fecha"]} - Autor: {libro["autor"]}')
+                else:
+                    print('Error: Libro no encontrado')
+            pausar_ejecucion()
         elif opcion == 4:
-            print(mostrar(libros))
-            congelar()
+            if verificar(libros):
+                print('Error: No hay libros en la biblioteca')
+            else:
+                print(mostrar_libros(libros))
+            pausar_ejecucion()
         elif opcion == 5:
-            titulo = input("Ingrese el titulo del libro: ")
-            print(eliminar(titulo, libros))
-            congelar()
+            if verificar(libros):
+                print('Error: No hay libros en la biblioteca')
+            else:
+                titulo = input("Ingrese el titulo del libro: ")
+                print(eliminar_libro(titulo, libros))
+            pausar_ejecucion()
         elif opcion == 6:
-            guardar("libros.json", libros)
+            guardar_archivo("libros.json", libros)
             print("Adios")
             break
         else:
             print("Error: Opción no válida")
-
 
 
 
